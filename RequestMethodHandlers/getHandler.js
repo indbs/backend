e8a9bfe1fc586849363d498327ac5a0cf0468e4d
Boris <b.smirnov@rusgates.ru>
2019-08-26 08:32:39 +0300

    handleGETrequest из getHandler в промисы + чейнинг. Читаемость лучше.

--- RequestMethodHandlers/getHandler.js
+++ RequestMethodHandlers/getHandler.js
@@ -14,14 +14,14 @@ export function handleGETrequest(req, connection) {
       if (jwt.verify(req.headers.authorization, tokens.client_side_token)==tokens.client_side_value){ 
         var inputQueryParams = parse(req.url, true).query;
         readMySQLQuery('./queries/query_' + Object.keys(inputQueryParams)[0] + '_' + inputQueryParams[Object.keys(inputQueryParams)[0]] + '.sql')
-        .then(text_query => {
-          queryMySQLConnection(connection, text_query, [inputQueryParams.year, inputQueryParams.program_number ? inputQueryParams.program_number : inputQueryParams.channel_number])
-            .then(results => {resolve(prepareAnswer(200, JSON.stringify(results), 'application/json'))})
-            .catch(err => {reject(prepareAnswer(200, err.toString(), 'text/html'))})
-        })
-        .catch(err => {
-          reject(prepareAnswer(200, err.toString(), 'text/html'));
-        })		
+        .then(
+          text_query  => queryMySQLConnection(connection, text_query, [inputQueryParams.year, inputQueryParams.program_number ? inputQueryParams.program_number : inputQueryParams.channel_number]),
+          errRead     => reject(prepareAnswer(200, errRead.toString(), 'text/html'))
+        )
+        .then(
+          results     => resolve(prepareAnswer(200, JSON.stringify(results), 'application/json')),
+          errQuery    => reject(prepareAnswer(200, errQuery.toString(), 'text/html'))
+        )		
       }
     }
     else 
--- fs/readQueryFromFile.js
+++ fs/readQueryFromFile.js
@@ -2,8 +2,8 @@ import { readFile } 																		from 'fs';
 
 export function readMySQLQuery(path){
   return new Promise((resolve, reject) => {
-    readFile(path, 'utf-8', (err, text_query) => {
-      if (err) reject(err);
+    readFile(path, 'utf-8', (errRead, text_query) => {
+      if (errRead) reject(errRead);
       else resolve(text_query);
     })
   });

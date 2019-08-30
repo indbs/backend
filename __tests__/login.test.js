e495a4e1c8b0014cd4f2033eff1b1fb5211435f1
Boris <b.smirnov@rusgates.ru>
2019-08-30 15:02:40 +0300

    Для возможности тестирования отдельных функций функции запроса, соединений и пр. вынесены из тела callback createServer.listen()

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,11 +1,13 @@
 import { tokens }																				from '../constants/tokens'
 import 	 jwt 																						from 'jsonwebtoken';
+import   pbkdf2                                         from 'crypto-js/pbkdf2';
+import   readQuery                                      from '../server'
 
 describe('loginTest', () => {
   it('userLogin', test_login('b.smirnov@rusgates.ru', 'password'))
 })
 
-function test_login(password){
+function test_login(email, password){
   var hashedPassword = pbkdf2(password, tokens.client_side_salt_hash, { keySize: 512/32, iterations: 1000 }).toString();
   var userDataPairToken = jwt.sign({email: email, hash: hashedPassword}, tokens.client_side_salt);
   const requestOptions = {
@@ -15,14 +17,21 @@ function test_login(password){
     },
     body: JSON.stringify({userDataPairToken})
   }
-  expects(
-    fetch(`http://172.16.20.75:8060/`, requestOptions)
-      .then((response)=>{
-        response.text()
-          .then(text => {
-            const data = text && JSON.parse(text);
-            return data;
-          })
-      })
-  ).toEqual({user: 'test'})
+
+  const results = readQuery(requestOptions);
+  expect(results).toEqual({user: 'test'})
+  done();
+  /*
+  const results = fetch(`http://172.16.20.75:8060/`, requestOptions)
+    .then((response)=>{
+      response.text()
+        .then(text => {
+          const data = text && JSON.parse(text);
+          return data;
+        })
+    })
+  expect(results).toEqual({user: 'test'})
+  done();
+  */
+
 }
--- server.js
+++ server.js
@@ -6,64 +6,71 @@ import { handleOPTIONSrequest }													from './RequestMethodHandlers/option
 import { handleGETrequest }															from './RequestMethodHandlers/getHandler'
 import { handlePOST_RegistrationRequest,
 				 handlePOST_LoginRequest}												from './RequestMethodHandlers/postHandler'
-  
+	
+function createCon(){
+	return createMySQLConnection();
+}			
+
+function makeCon(req, res, con){
+	return makeMySQLConnection(con)
+		.then(
+			readQuery(req, res, con),
+			(error) => writeAnswer(res, error)
+		)
+}
+
+export function readQuery(req, res, connection){
+	if (req.method == 'POST') {
+		let body = [];
+		req.on('data', (chunk)=>{
+			body.push(chunk);
+		}).on('end', () => {																														//receiving body data from http
+			body = Buffer.concat(body).toString();
+			if (JSON.parse(body).userDataPairToken){
+				handlePOST_LoginRequest(body, connection)
+					.then(
+						(result) 	=> writeAnswer(req, res, result),
+						(error) 	=> writeAnswer(req, res, error)
+					)
+			}
+			if (JSON.parse(body).userDataQuartetToken){
+				handlePOST_RegistrationRequest(body, connection)
+					.then(
+						(result) 	=> writeAnswer(req, res, result),
+						(error) 	=> writeAnswer(req, res, error)
+					)
+			}
+		});
+	}
+	
+	if (req.method == 'GET') {
+		handleGETrequest(req, connection)
+			.then(
+				(result) 	=> writeAnswer(req, res, result),
+				(error) 	=> writeAnswer(req, res, error)
+			)
+	}
+
+	if (req.method == 'OPTIONS') {
+	handleOPTIONSrequest(req, res)
+		.then(
+			(result) 		=> writeAnswer(req, res, result),
+			(error) 		=> writeAnswer(req, res, error)
+		)
+	}
+}
+
 createServer(function (req, res) {
 	
-	var connection = createMySQLConnection();
+	var connection = createCon();
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type, access-control-allow-origin");
 
-	makeMySQLConnection(connection)
-		.then(
-			readQuery(req),
-			(error) => writeAnswer(error.responseCode, error.responseResult, error.responseType)
-		)
-
-	function readQuery(req){
-		if (req.method == 'POST') {
-			let body = [];
-			req.on('data', (chunk)=>{
-				body.push(chunk);
-			}).on('end', () => {																														//receiving body data from http
-				body = Buffer.concat(body).toString();
-				if (JSON.parse(body).userDataPairToken){
-					handlePOST_LoginRequest(body, connection)
-						.then(
-							(result) 	=> writeAnswer(result.responseCode, result.responseResult, result.responseType),
-							(error) 	=> writeAnswer(error.responseCode, error.responseResult, error.responseType)
-						)
-				}
-				if (JSON.parse(body).userDataQuartetToken){
-					handlePOST_RegistrationRequest(body, connection)
-						.then(
-							(result) 	=> writeAnswer(result.responseCode, result.responseResult, result.responseType),
-							(error) 	=> writeAnswer(error.responseCode, error.responseResult, error.responseType)
-						)
-				}
-			});
-		}
-		
-		if (req.method == 'GET') {
-			handleGETrequest(req, connection)
-				.then(
-					(result) 	=> writeAnswer(result.responseCode, result.responseResult, result.responseType),
-					(error) 	=> writeAnswer(error.responseCode, error.responseResult, error.responseType)
-				)
-		}
-
-		if (req.method == 'OPTIONS') {
-		handleOPTIONSrequest(req, res)
-			.then(
-				(result) 		=> writeAnswer(result.responseCode, result.responseResult, result.responseType),
-				(error) 		=> writeAnswer(error.responseCode, error.responseResult, error.responseType)
-			)
-		}
-	}
-
-	function writeAnswer(status, argumentText, argumentType){
-		res.writeHead(status, {'Content-Type': argumentType});
-		res.end(argumentText);	
-		closeMySQLConnection(connection);
-	}
+	makeCon(req, res, connection);
 
 }).listen(8060);
+
+function writeAnswer(req, res, content){
+	res.writeHead(content.responseCode, {'Content-Type': content.responseType});
+	res.end(content.responseResult);	
+}
